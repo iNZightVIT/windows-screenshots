8a3f3b8c1795b55fe06769d93c2154f25c65f343
Tom Elliott <tom.elliott@auckland.ac.nz>
2022-01-17 22:48:03 +1300

    Add rect/circle/text/arrows and crop images

--- 02_user_guides/03_file_menu/01_import_data.R
+++ 02_user_guides/03_file_menu/01_import_data.R
@@ -6,11 +6,27 @@ capture("01_import_file")
 
 capture("02_import_browse")
 crop("02_import_browse", y1 = 200)
-add_rect("02_import_browse", 478, 190, 68, 34,
+add_rect("02_import_browse", 478, 102, 664, 70,
     border_width = 3L)
 
 w$loadURL$set_value(TRUE)
 capture("03_import_url")
 crop("03_import_url", y1 = 200)
+
+add_arrow("03_import_url", 45, 115, 90,
+    arrow_width = 3L
+)
+add_circle("03_import_url", 25, 115, 5, width = 3L)
+add_text("03_import_url", 25, 115, "1", font_size = 1.8)
+
+add_arrow("03_import_url", 300, 82, 220,
+    arrow_width = 3L
+)
+add_circle("03_import_url", 320, 82, 5, width = 3L)
+add_text("03_import_url", 320, 82, "2", font_size = 1.8)
+add_text("03_import_url", 340, 82, 
+    "Type/paste in URL", font_size = 1.2,
+    justify_x = 0)
+
 
 w$close()
--- screenshots.R
+++ screenshots.R
@@ -58,7 +58,6 @@ crop <- function(file,
         "%s.png",
         file.path(dir, file)
     )
-    message('Editing ', name, ' ...')
     if (!file.exists(name)) stop("File does not exist")
 
     img <- magick::image_read(name)
@@ -73,7 +72,7 @@ crop <- function(file,
     invisible(NULL)
 }
 
-add_rect <- function(file, x0, width, y0, height,
+add_rect <- function(file, x0, y0, x1, y1,
                      border_col = "red", border_width = 2,
                      fill_col = NA) {
     if (missing(file)) stop("You need to specify a path")
@@ -83,15 +82,96 @@ add_rect <- function(file, x0, width, y0, height,
         "%s.png",
         file.path(dir, file)
     )
-    message('Editing ', name, ' ...')
     if (!file.exists(name)) stop("File does not exist")
 
     img <- magick::image_read(name)
 
     img_e <- magick::image_draw(img)
-    rect(x0, y0, x0 + width, y0 + height,
+    rect(x0, y0, x1, y1,
         border = border_col, lwd = border_width,
         bg = fill_col
+    )
+    dev.off()
+    magick::image_write(img_e, name)
+}
+
+add_arrow <- function(file, x0, y0, x1 = x0, y1 = y0,
+                      arrow_col = "red",
+                      arrow_width = 2L,
+                      head_angle = 30,
+                      head_length = 0.2
+                      ) {
+    if (missing(file)) stop("You need to specify a path")
+    dir <- "images"
+    if (DIR != "") dir <- file.path(dir, DIR)
+    name <- sprintf(
+        "%s.png",
+        file.path(dir, file)
+    )
+    if (!file.exists(name)) stop("File does not exist")
+
+    img <- magick::image_read(name)
+
+    img_e <- magick::image_draw(img)
+    arrows(x0, y0, x1, y1,
+        length = head_length,
+        angle = head_angle,
+        lwd = arrow_width,
+        col = arrow_col
+    )
+    dev.off()
+    magick::image_write(img_e, name)
+}
+
+add_circle <- function(file, x0, y0, radius,
+                       col = "red",
+                       width = 2L,
+                       fill = NA) {
+    if (missing(file)) stop("You need to specify a path")
+    dir <- "images"
+    if (DIR != "") dir <- file.path(dir, DIR)
+    name <- sprintf(
+        "%s.png",
+        file.path(dir, file)
+    )
+    if (!file.exists(name)) stop("File does not exist")
+
+    img <- magick::image_read(name)
+
+    img_e <- magick::image_draw(img)
+    points(x0, y0, 
+        pch = 21,
+        lwd = width,
+        col = col,
+        cex = radius
+    )
+    dev.off()
+    magick::image_write(img_e, name)
+}
+
+add_text <- function(file, x0, y0, text, 
+                     font_col = "red",
+                     font_size = 1,
+                     font_style = 1L,
+                     justify_x = 0.5,
+                     justify_y = NA) {
+    if (missing(file)) stop("You need to specify a path")
+    dir <- "images"
+    if (DIR != "") dir <- file.path(dir, DIR)
+    name <- sprintf(
+        "%s.png",
+        file.path(dir, file)
+    )
+    if (!file.exists(name)) stop("File does not exist")
+
+    img <- magick::image_read(name)
+
+    img_e <- magick::image_draw(img)
+    text(x0, y0, labels = text,
+        col = font_col,
+        cex = font_size,
+        font = font_style,
+        adj = c(justify_x, justify_y)
     )
     dev.off()
     magick::image_write(img_e, name)
@@ -108,7 +188,7 @@ capture("00_inzight_init")
 
 # ----- landing page
 sections <- list.files(pattern = "[0-9]{2}\\_.+")
-for (section in sections[2]) {
+for (section in sections) {
     DIR <- section
     source(file.path(DIR, "main.R"))
 }
